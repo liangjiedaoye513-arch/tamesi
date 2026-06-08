bed7a86d3802d6deca6fa4eb1e3bd025026c9e1c
liangjiedaoye513gmail.com <liangjiedaoye513@gmail.com>
2026-06-08 10:37:04 +0900

    java初めてのコミット

---
+++ src/tamesi/d.java
@@ -0,0 +1,19 @@
+package tamesi;
+
+import java.util.LinkedList;
+
+public class d {
+	public static void main(String[] args) {
+		
+	
+LinkedList<String> ll = new LinkedList<String>();
+
+ll.add("1");
+ll.addLast("2");
+ll.addFirst("3");
+
+for(String s:ll) {
+	System.out.println(s);
+}
+}
+}
